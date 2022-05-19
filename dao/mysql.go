fae4bd894c5237363ed64c5aa8d15e795cf69b0f
Latecoming <92357901+CloserLu@users.noreply.github.com>
2022-05-19 19:32:10 +0800

    Add files via upload

---
+++ dao/mysql.go
@@ -0,0 +1,29 @@
+package dao
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type TodoList struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status bool   `json:"status"`
+}
+
+var DB *gorm.DB
+var err error
+
+func InitDb() {
+	dsn := "root:20021020@tcp(127.0.0.1:3306)/bubble?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open("mysql", dsn)
+	if err != nil {
+		return
+	}
+	//禁用默认表名的复数形式
+	DB.SingularTable(true)
+	//迁移建表
+	DB.AutoMigrate(&TodoList{})
+	//设置连接池中的最大闲置连接数
+	DB.DB().SetMaxOpenConns(10)
+}
